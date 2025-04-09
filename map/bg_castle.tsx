a9c7d628cacb88dfa5c983b988fa16a99d876ad4
clemanratpi <cottel.clement@gmail.com>
2025-04-09 17:46:52 +0200

    Add files via upload

---
+++ map/bg_castle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="bg_castle" tilewidth="18" tileheight="18" spacing="1" tilecount="1431" columns="53">
+ <image source="../../../../Downloads/Backgrounds/bg_castle.png" width="1024" height="512"/>
+</tileset>
---
+++ map/bg_shroom.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="bg_shroom" tilewidth="18" tileheight="18" spacing="8" tilecount="780" columns="39">
+ <image source="../../../../Downloads/Backgrounds/bg_shroom.png" width="1024" height="512"/>
+</tileset>
